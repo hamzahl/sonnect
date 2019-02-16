5096aef6c64b656df9778aaa397f8af684a41aaa
Hamzah Lorgat <hamzah_97@hotmail.com>
2019-02-16 03:21:08 +0000

    Update console text that displays when Server is running

--- app.js
+++ app.js
@@ -24,4 +24,4 @@ mongoose.connect(keys.mongoUri, { useNewUrlParser: true })
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
